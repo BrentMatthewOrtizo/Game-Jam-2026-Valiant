56f161f2133ff948348bcb9195719acb6d5b1bc0
Brent Matthew Ortizo <144473184+BrentMatthewOrtizo@users.noreply.github.com>
2026-02-06 17:02:14 -0800

    Added coyote time functionality

--- Assets/Scripts/PlayerMovement2D.cs
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -11,11 +11,17 @@ public class PlayerMovement2D : MonoBehaviour
     [Header("Jump")]
     [SerializeField] private float jumpForce = 14f;
 
+    [Header("Jump Forgiveness")]
+    [SerializeField] private float coyoteTime = 0.10f;     // jump shortly after leaving ground - added coyote
+    [SerializeField] private float jumpBufferTime = 0.10f; // jump shortly before landing
+
     private Rigidbody2D rb;
     private PlayerCollision2D coll;
 
     private Vector2 moveInput;
-    private bool jumpQueued;
+
+    private float coyoteTimer;
+    private float jumpBufferTimer;
 
     private void Awake()
     {
@@ -23,32 +29,42 @@ public class PlayerMovement2D : MonoBehaviour
         coll = GetComponent<PlayerCollision2D>();
     }
 
+    private void Update()
+    {
+        if (coll.OnGround)
+            coyoteTimer = coyoteTime;
+        else
+            coyoteTimer -= Time.deltaTime;
+
+        jumpBufferTimer -= Time.deltaTime;
+    }
+
     private void FixedUpdate()
     {
+        // horizontal movement
         rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
-        
-        if (jumpQueued)
-        {
-            jumpQueued = false;
 
-            if (coll.OnGround)
-            {
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
-                rb.linearVelocity += Vector2.up * jumpForce;
-            }
+        // if we pressed jump recently and we are grounded or within coyote window
+        if (jumpBufferTimer > 0f && coyoteTimer > 0f)
+        {
+            jumpBufferTimer = 0f;
+            coyoteTimer = 0f;
+
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+            rb.linearVelocity += Vector2.up * jumpForce;
         }
     }
-    
+
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
-    
+
     public void OnJump(InputValue value)
     {
         if (value.isPressed)
         {
-            jumpQueued = true;
+            jumpBufferTimer = jumpBufferTime;
         }
     }
 }
